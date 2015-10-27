2134644d3fd8f6f0e6d7df13c1f8f3272139ad6b
Matthew Williams <mwilliamsnov6@gmail.com>
2015-10-27 14:34:32 -0700

    inital commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("FundApp", []);
+// app.config(['$routeProvider', '$httpProvider',  function($routeProvider, $httpProvider) {
+//     $routeProvider
+//       .when('/stock', {
+//         templateUrl: 'partials/home.html',
+//         controller: 'StockController'
+//       })
+//       .when('/game', {
+//         templateUrl: 'partials/game.html',
+//         controller: 'CardController'
+//       })
+// }])
+
+
+
---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+app.controller('FundController', ["$scope", function($scope){
+  $scope.test = function() {
+    return "foo"
+  }
+
+  console.log("foo");
+
+}]) // END CONTROLLER
