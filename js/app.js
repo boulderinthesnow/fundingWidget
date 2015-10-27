661d51cf65aec61257e30b479219a7f3fc62c448
Matthew Williams <mwilliamsnov6@gmail.com>
2015-10-27 15:16:38 -0700

    outline for boxes and tooltip created

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
 var app = angular.module("FundApp", []);
-// app.config(['$routeProvider', '$httpProvider',  function($routeProvider, $httpProvider) {
-//     $routeProvider
-//       .when('/stock', {
-//         templateUrl: 'partials/home.html',
-//         controller: 'StockController'
-//       })
-//       .when('/game', {
-//         templateUrl: 'partials/game.html',
-//         controller: 'CardController'
-//       })
-// }])
+
 
 
 
