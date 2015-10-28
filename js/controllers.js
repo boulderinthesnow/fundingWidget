1fb1fa721674d7f7da1fd9cec43566345505cc15
Matthew Williams <mwilliamsnov6@gmail.com>
2015-10-28 14:19:16 -0700

    widget logic for tool tip finished

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,36 @@
 app.controller('FundController', ["$scope", function($scope){
 
 	// starting contribution amount	
-	$scope.total = 0
+	var total = 0
+
+	// starting amount needed
+	$scope.remaining = 500
+
 
 	// this function adds money to the total donation amount,
 	// modifies progress bar,
 	// and clears donation input field
 	$scope.addMoney = function () {
-		$scope.total += parseInt($scope.donation)
-		$scope.donation = undefined;
-		var percentDone = ($scope.total / 500) * 100
-		$("#progress-bar").css("width",percentDone+"%")
+		if (parseInt($scope.donation)) {     
+			total += parseInt($scope.donation)
+			$scope.remaining -= parseInt($scope.donation)
+			$scope.donation = undefined;
+			var percentDone = (total / 500) * 100
+			$("#progress-bar").css("width",percentDone+"%")
+		}
 	}
+
+	$scope.progressBarHoverOn = function () {
+		$('#tool-tip-container').css('opacity','1')
+	}
+
+	$scope.progressBarHoverOff = function () {
+		$('#tool-tip-container').css('opacity','0')
+	}
+
+	$("#friends-button").click(function () {
+		window.open('http://www.auction.com/')
+	})
 
 
 }]) // END CONTROLLER
