057fa64ad7699636bca5e773bf3fbcc5347890c8
Matthew Williams <mwilliamsnov6@gmail.com>
2015-10-28 12:30:53 -0700

    progess bar changes with donation amounts

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,17 @@
 app.controller('FundController', ["$scope", function($scope){
-  $scope.test = function() {
-    return "foo"
-  }
 
-  console.log("foo");
+	// starting contribution amount	
+	$scope.total = 0
+
+	// this function adds money to the total donation amount,
+	// modifies progress bar,
+	// and clears donation input field
+	$scope.addMoney = function () {
+		$scope.total += parseInt($scope.donation)
+		$scope.donation = undefined;
+		var percentDone = ($scope.total / 500) * 100
+		$("#progress-bar").css("width",percentDone+"%")
+	}
+
 
 }]) // END CONTROLLER
